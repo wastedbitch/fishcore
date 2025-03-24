99a84064853d121eb7376267a91dd2a15e14f484
fish <wastedbxtch@gmail.com>
2025-03-24 20:06:12 +0100

    bugfix meow

--- dev.sh
+++ dev.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+!/bin/bash
 
-# echo "Stopping old dev container"
-# docker stop devcore 2>/dev/null || true
+echo "Stopping old dev container"
+docker stop devcore 2>/dev/null || true
 
-# echo "Removing old dev container"
-# docker rm devcore 2>/dev/null || true
+echo "Removing old dev container"
+docker rm devcore 2>/dev/null || true
 
-# echo "Removing old dev image"
-# docker image rm devcore 2>/dev/null || true
+echo "Removing old dev image"
+docker image rm devcore 2>/dev/null || true
 
 echo "Building new dev container from Devfile"
 docker build --no-cache -f DevDockerfile -t devcore .
@@ -15,4 +15,4 @@ docker build --no-cache -f DevDockerfile -t devcore .
 echo "Starting dev container with docker compose"
 docker compose -f dev-docker-compose.yaml up -d
 
-docker exec -it devcore sh
+# docker exec -it devcore sh
